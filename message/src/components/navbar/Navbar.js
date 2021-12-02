9cdf18ffa96eddf47f866cdd1d99e0f3e6717d3d
manchala1 <88335054+manchala1@users.noreply.github.com>
2021-12-02 19:25:51 +0530

    added message frontEnd and socket backend

---
+++ message/src/components/navbar/Navbar.js
@@ -0,0 +1,62 @@
+import React, { useState, useEffect } from "react";
+import "./navbar.css";
+import Notification from "../../img/notification.svg";
+import Message from "../../img/message.svg";
+import Settings from "../../img/settings.svg";
+
+const Navbar = ({ socket }) => {
+  const [notifications, setNotifications] = useState([]);
+  const [open, setOpen] = useState([]);
+  useEffect(() => {
+    socket.on("getNotification", (data) => {
+      setNotifications((prev) => [...prev, data]);
+    });
+  }, [socket]);
+  const displayNotification = ({ senderName, type }) => {
+    let action;
+
+    if (type === 1) {
+      action = "liked";
+    } else if (type === 2) {
+      action = "commented";
+    } else {
+      action = "shared";
+    }
+    return (
+      <span className="notification">{`${senderName} ${action} your post.`}</span>
+    );
+  };
+  const handleRead = () => {
+    setNotifications([]);
+    setOpen(false);
+  };
+  return (
+    <div className="navbar">
+      <span className="logo">Jahnavi App</span>
+      <div className="icons">
+        <div className="icon" onClick={() => setOpen(!open)}>
+          <img src={Notification} className="iconImg" alt="" />
+          {notifications.length > 0 && (
+            <div className="counter">{notifications.length}</div>
+          )}
+        </div>
+        <div className="icon" onClick={() => setOpen(!open)}>
+          <img src={Message} className="iconImg" alt="" />
+        </div>
+        <div className="icon" onClick={() => setOpen(!open)}>
+          <img src={Settings} className="iconImg" alt="" />
+        </div>
+        {open && (
+          <div className=" notifications">
+            {notifications.map((n) => displayNotification(n))}
+            <button className="nbutton" onClick={handleRead}>
+              Mark as read
+            </button>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
